146ef5b6edd4ff9886516422ff42276cad5f3290
David North <dmhnorth@gmail.com>
2014-08-12 13:32:05 +0100

    Initial commit

---
+++ SparkServer/src/main/java/HelloWorld.java
@@ -0,0 +1,20 @@
+import spark.Request;
+import spark.Response;
+
+import static spark.Spark.*;
+
+
+/**
+ * Spark defaults to port:4567
+ *
+ * type 'curl http://localhost:4567/hello' into terminal to test
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        get("/hello", (Request req, Response res) -> {
+            return "Hello World";
+        });
+
+    }
+}
